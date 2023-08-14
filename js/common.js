7fd04fa26c24a767a2d61fbae902cb814a2180a6
tina <tina.tang@ddiworld.com>
2023-08-14 22:47:48 +0800

    ttt:: 20230814

---
+++ js/common.js
@@ -0,0 +1,5 @@
+// header
+$("#header").append(
+        "<div class='mheadnav'><img src='./img/mlogo.png' alt='' srcset='' class='mlogo tabimg1'></div>"+
+        "<div class='mheadnav'><img src='./img/tab.png' alt='' srcset='' class='mlogo tabimg2'></div>"
+)
