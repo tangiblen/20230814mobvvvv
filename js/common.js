eb67756b594671e5e656826891a0649a728fe74e
tina <tina.tang@ddiworld.com>
2023-08-15 15:06:47 +0800

    ttt:: 首页完成

--- js/common.js
+++ js/common.js
@@ -1,5 +1,26 @@
 // header
 $("#header").append(
         "<div class='mheadnav'><img src='./img/mlogo.png' alt='' srcset='' class='mlogo tabimg1'></div>"+
-        "<div class='mheadnav'><img src='./img/tab.png' alt='' srcset='' class='mlogo tabimg2'></div>"
-)
+        "<div class='mheadtab'><img src='./img/tab.png' alt='' srcset='' class='mlogo tabimg2'></div>"
+);
+
+$("#footer").append(
+        "<div class='footertrans'>"+
+                    "<span class='dotblue ju' style='display: inline-block; padding-top: 10px;'>联系我们</span><br />"+
+                "<div class='footercontent'>"+
+                    "<div class='footercontent-left'>"+
+                        "<span class='dotblue ju'>电话</span>"+
+                        "<span class='dotblue ju'>邮箱</span>"+
+                        "<span class='dotblue ju'>网址</span>"+
+                        "<span class='dotblue ju'>地址</span>"+
+                    "</div>"+
+                    "<div class='footercontent-right'>"+
+                        "021-64030698<br />"+
+                        "marketing@datageo.cn<br />"+
+                        "http://www.datageo.cn<br />"+
+                        "上海市徐汇区云锦路700号西岸智塔西塔32层3208室"+
+                    "</div>"+
+                "</div>"+
+            "</div>"+
+            "<div style='font-family: w1;font-size: 2.6666vw;color: #ffffff;text-align: center;'>上海数阖信息技术有限公司© 2018-2023  版权所有 沪ICP备2023002396号-1</div>"
+);
